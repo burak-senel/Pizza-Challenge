aa8f7ea90be1f37de7794c78296ec9c383eebdb4
burak-senel <buurak.senel@gmail.com>
2024-03-09 12:25:57 +0300

    feature (home component without css)

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <div>
+            <img src="./Assets/mile1-assets/home-banner.png" />
+            <img src="./Assets/mile1-assets/logo.svg" />
+            <p>KOD ACIKTIRIR PİZZA DOYURUR</p>
+            <button >sdfdsf</button>
+        </div>
+    )
+
+
+}
