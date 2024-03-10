aac140a0ca867a8085c76ff6248543458f4e3ccc
burak-senel <buurak.senel@gmail.com>
2024-03-10 16:24:51 +0300

    form done except js

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-
-import { Route, Router, Switch } from 'react-router-dom/cjs/react-router-dom.min'
-import './App.css'
-import Home from './components/Home'
-import Order from './components/Order'
+import {
+  Route,
+  Router,
+  Switch,
+} from "react-router-dom/cjs/react-router-dom.min";
+import "./App.css";
+import Home from "./components/Home";
+import Order from "./components/Order";
 
 function App() {
-
   return (
     <Switch>
       <Route exact path="/">
@@ -15,7 +17,7 @@ function App() {
         <Order />
       </Route>
     </Switch>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -9,9 +9,13 @@ export default function Order() {
       <section className="main-section">
         <div className="product">
           <h3>Position Absolute Acı Pizza</h3>
-          <h2>85.50₺</h2>
-          <span>4.9</span>
-          <span>(200)</span>
+          <div className="price">
+            {" "}
+            <h2>85.50₺</h2>
+            <span className="rate">4.9</span>
+            <span>(200)</span>
+          </div>
+
           <p>
             Frontend Dev olarak hala position:absolute kullanıyorsan bu çok acı
             pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli
@@ -24,10 +28,11 @@ export default function Order() {
         </div>
         <form>
           <fieldset>
-            <h3>
-              Boyut Seç<span>*</span>
-            </h3>
             <div className="product-size">
+              <h3>
+                Boyut Seç<span className="yildiz"> *</span>
+              </h3>
+
               <label>
                 <input type="radio" name="size" value="kucuk" />
                 Küçük
@@ -44,9 +49,12 @@ export default function Order() {
 
             <div className="product-pastry">
               <h3 htmlFor="pastry">
-                Hamur Seç<span>*</span>
+                Hamur Seç<span className="yildiz"> *</span>
               </h3>
               <select id="pastry" name="pastry">
+                <option value="" selected disabled hidden>
+                  Hamur Kalınlığı
+                </option>
                 <option value="thin">İnce</option>
                 <option value="standard">Standart</option>
                 <option value="thick">Kalın</option>
@@ -58,13 +66,64 @@ export default function Order() {
             <p>
               En az 4, en fazla 10 malzeme seçebilirsiniz. (Her biri ekstra 5₺)
             </p>
-            {malzemeler.map((malzeme, index) => (
-              <div className="additions" key={index}>
-                <input type="checkbox" id={malzeme} name={malzeme} />
-                <label htmlFor={malzeme}>{malzeme}</label>
-              </div>
-            ))}
+            <div className="checkbox-container">
+              {malzemeler.map((malzeme, index) => (
+                <div className="additions" key={index}>
+                  <input type="checkbox" id={malzeme} name={malzeme} />
+                  <label className="checkbox-label" htmlFor={malzeme}>
+                    {malzeme}
+                  </label>
+                </div>
+              ))}
+            </div>
           </div>
+          <section className="infos">
+            <div className="name">
+              {" "}
+              <h3>Adınız:</h3>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Siparişini teslim edebilmemiz için adını gir"
+              />
+            </div>
+            <div className="note">
+              {" "}
+              <h3>Sipariş Notu</h3>
+              <input
+                type="text"
+                name="note"
+                id="note"
+                placeholder="Siparişine eklemek istediğin bir not var mı?"
+              />
+            </div>
+          </section>
+          <div className="seperator"></div>
+          <section className="order-spec">
+            <div className="orderCount">
+              <button>-</button>
+              <span>total sipariş</span>
+              <button>+</button>
+            </div>
+            <div className="orderSum-container">
+              {" "}
+              <div className="orderSum">
+                <h3>Sipariş Toplamı</h3>
+                <div className="orderTotal">
+                  <div className="orderTotal-left">
+                    <p>Seçimler</p>
+                    <p>Toplam</p>
+                  </div>
+                  <div className="orderTotal-right">
+                    <p>addition total</p>
+                    <p>genel total</p>
+                  </div>
+                </div>
+              </div>
+              <button className="order-button">SİPARİŞ VER</button>
+            </div>
+          </section>
         </form>
       </section>
     </>
