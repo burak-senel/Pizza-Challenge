daf901eccde745fd20d8e8aff56008d4693d5532
burak-senel <buurak.senel@gmail.com>
2024-03-12 12:41:58 +0300

    feature (homepage-button-animaton added)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,27 @@
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import gif from "./gif.gif"; // GIF dosyasının yolu
 import "./Home.css";
+
 export default function Home() {
+  const [showGif, setShowGif] = useState(false);
+
   return (
     <div className="hero-page">
       <div className="hero-container">
-        <img className="logo" src="./Assets/mile1-assets/logo.svg" />
+        <img className="logo" src="./Assets/mile1-assets/logo.svg" alt="Logo" />
         <p style={{ fontFamily: "Barlow Condensed", fontWeight: "100" }}>
-          KOD ACIKTIRIR <br></br> PIZZA DOYURUR
+          KOD ACIKTIRIR <br /> PIZZA DOYURUR
         </p>
-        <Link to="./OrderPizza">
-          <button>ACIKTIM</button>
+        <Link to="./OrderPizza" className="button-link">
+          <button
+            className="button"
+            onMouseEnter={() => setShowGif(true)}
+            onMouseLeave={() => setShowGif(false)}
+          >
+            ACIKTIM
+            <img src={gif} className="gif" alt="GIF" />
+          </button>
         </Link>
       </div>
     </div>
