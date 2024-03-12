aa86bd07fc520b9a7d673e184a879faa7be4d235
burak-senel <buurak.senel@gmail.com>
2024-03-12 09:12:44 +0300

    home page css done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,17 @@
-import { Link } from "react-router-dom/cjs/react-router-dom.min"
-import "./Home.css"
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import "./Home.css";
 export default function Home() {
-    return (
-        <div className="hero-container">
-            <img className="hero-img" src="./Assets/mile1-assets/home-banner.png" />
-            <img src="./Assets/mile1-assets/logo.svg" />
-            <p className="text-3xl">KOD ACIKTIRIR PİZZA DOYURUR</p>
-            <Link to="./OrderPizza">
-                <button className="overlay-button">sdfdsf</button>
-            </Link>
-        </div>
-    )
-
-
+  return (
+    <div className="hero-page">
+      <div className="hero-container">
+        <img className="logo" src="./Assets/mile1-assets/logo.svg" />
+        <p style={{ fontFamily: "Barlow Condensed", fontWeight: "100" }}>
+          KOD ACIKTIRIR <br></br> PIZZA DOYURUR
+        </p>
+        <Link to="./OrderPizza">
+          <button>ACIKTIM</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
